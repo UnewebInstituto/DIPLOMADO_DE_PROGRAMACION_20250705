9ed4793bc7d76cbcce86bd34db54e8d24a071dac
Aaron D. Duque R. <duqueaaron022006@gmail.com>
2025-12-10 11:59:18 -0400

    clase 3 Python nivel I

---
+++ PYTHON_NIVEL_I/PROFESOR/modulo_calculadora.py
@@ -0,0 +1,15 @@
+# DECLARACIÓN DE FUNCIONES
+def sumar(arg_n1, arg_n2):
+    return arg_n1 + arg_n2
+
+def restar(arg_n1, arg_n2):
+    return arg_n1 - arg_n2
+
+def multiplicar(arg_n1, arg_n2):
+    return arg_n1 * arg_n2
+
+def dividir(arg_n1, arg_n2):
+    if arg_n2 == 0:
+        return 'ERROR DIVISIÓN POR CERO'
+    else:
+        return arg_n1 / arg_n2
